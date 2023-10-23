c32f1b07b9e9591450a93107ce7bab0bec0ca4c9
Emmanuel <ebriones@miners.utep.edu>
2023-10-23 17:17:10 -0600

    testing strncmp

--- ptrace.c
+++ ptrace.c
@@ -2,15 +2,16 @@
 #include "user.h"
 #include "syscall.h"
 #include "ptrace.h"
+#include "defs.h"
 
 int ptrace(int mask) {
     if (mask == 0){
-        printf(2, "ptrace failed\n");
+        cprintf("ptrace failed\n");
         return 0;
     }
     // Tracing logic goes here
     else {
-        printf(2, "Mask: %d\n", mask);
+        cprintf("Mask: %d\n", mask);
         return mask;
     }
 }
@@ -19,50 +20,50 @@ int call_to_mask(const char *syscall_name) {
     int mask = 0; // Initialize the mask to 0
 
     // Map the syscall name to the corresponding syscall number (mask) using a switch statement
-    if (strcmp(syscall_name, "fork") == 0) {
+    if (strncmp(syscall_name, "fork", 4) == 0) {
         mask = SYS_fork;
-    } else if (strcmp(syscall_name, "exit") == 0) {
+    } else if (strncmp(syscall_name, "exit", 5) == 0) {
         mask = SYS_exit;
-    } else if (strcmp(syscall_name, "wait") == 0) {
+    } else if (strncmp(syscall_name, "wait", 5) == 0) {
         mask = SYS_wait;
-    } else if (strcmp(syscall_name, "pipe") == 0) {
+    } else if (strncmp(syscall_name, "pipe", 5) == 0) {
         mask = SYS_pipe;
-    } else if (strcmp(syscall_name, "read") == 0) {
+    } else if (strncmp(syscall_name, "read", 5) == 0) {
         mask = SYS_read;
-    }else if (strcmp(syscall_name, "kill") == 0) {
+    }else if (strncmp(syscall_name, "kill", 5) == 0) {
         mask = SYS_kill;
-    }else if (strcmp(syscall_name, "exec") == 0) {
+    }else if (strncmp(syscall_name, "exec", 5) == 0) {
         mask = SYS_exec;
-    }else if (strcmp(syscall_name, "fstat") == 0) {
+    }else if (strncmp(syscall_name, "fstat", 6) == 0) {
         mask = SYS_fstat;
-    }else if (strcmp(syscall_name, "chdir") == 0) {
+    }else if (strncmp(syscall_name, "chdir", 6) == 0) {
         mask = SYS_chdir;
-    }else if (strcmp(syscall_name, "dup") == 0) {
+    }else if (strncmp(syscall_name, "dup", 4) == 0) {
         mask = SYS_dup;
-    }else if (strcmp(syscall_name, "getpid") == 0) {
+    }else if (strncmp(syscall_name, "getpid", 7) == 0) {
         mask = SYS_getpid;
-    }else if (strcmp(syscall_name, "sbrk") == 0) {
+    }else if (strncmp(syscall_name, "sbrk", 5) == 0) {
         mask = SYS_sbrk;
-    }else if (strcmp(syscall_name, "sleep") == 0) {
+    }else if (strncmp(syscall_name, "sleep", 6) == 0) {
         mask = SYS_sleep;
-    }else if (strcmp(syscall_name, "uptime") == 0) {
+    }else if (strncmp(syscall_name, "uptime", 7) == 0) {
         mask = SYS_uptime;
-    }else if (strcmp(syscall_name, "open") == 0) {
+    }else if (strncmp(syscall_name, "open", 5) == 0) {
         mask = SYS_open;
-    }else if (strcmp(syscall_name, "write") == 0) {
+    }else if (strncmp(syscall_name, "write", 6) == 0) {
         mask = SYS_write;
-    }else if (strcmp(syscall_name, "mknod") == 0) {
+    }else if (strncmp(syscall_name, "mknod", 6) == 0) {
         mask = SYS_mknod;
-    }else if (strcmp(syscall_name, "unlink") == 0) {
+    }else if (strncmp(syscall_name, "unlink", 7) == 0) {
         mask = SYS_unlink;
-    }else if (strcmp(syscall_name, "link") == 0) {
+    }else if (strncmp(syscall_name, "link", 5) == 0) {
         mask = SYS_link;
-    }else if (strcmp(syscall_name, "mkdir") == 0) {
+    }else if (strncmp(syscall_name, "mkdir", 6) == 0) {
         mask = SYS_mkdir;
-    }else if (strcmp(syscall_name, "close") == 0) {
+    }else if (strncmp(syscall_name, "close", 6) == 0) {
         mask = SYS_close;
     }else{
-         printf(2, "Sys call does not exist\n");
+        cprintf("sys call does not exist\n");
     }
        
     return mask;
