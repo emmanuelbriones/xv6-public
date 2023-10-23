192c74813fc592400fb4bdc3740844acb250c7e1
Emmanuel <ebriones@miners.utep.edu>
2023-10-23 15:13:28 -0600

    fix

--- ptrace.c
+++ ptrace.c
@@ -2,7 +2,7 @@
 #include "user.h"
 #include "syscall.h"
 
-int ptrace(int mask) {
+int sys_ptrace(int mask) {
     if (mask == 0){
         printf(2, "ptrace failed\n");
         return 0;
@@ -18,79 +18,54 @@ int call_to_mask(const char *syscall_name) {
     int mask = 0; // Initialize the mask to 0
 
     // Map the syscall name to the corresponding syscall number (mask) using a switch statement
-    switch (syscall_name) {
-        case "fork":
-            mask = SYS_fork;
-            break;
-        case "exit":
-            mask = SYS_exit;
-            break;
-        case "wait":
-            mask = SYS_wait;
-            break;
-        case "pipe":
-            mask = SYS_pipe;
-            break;
-        case "read":
-            mask = SYS_read;
-            break;
-        case "kill":
-            mask = SYS_kill;
-            break;
-        case "exec":
-            mask = SYS_exec;
-            break;
-        case "fstat":
-            mask = SYS_fstat;
-            break;
-        case "chdir":
-            mask = SYS_chdir;
-            break;
-        case "dup":
-            mask = SYS_dup;
-            break;
-        case "getpid":
-            mask = SYS_getpid;
-            break;
-        case "sbrk":
-            mask = SYS_sbrk;
-            break;
-        case "sleep":
-            mask = SYS_sleep;
-            break;
-        case "uptime":
-            mask = SYS_uptime;
-            break;
-        case "open":
-            mask = SYS_open;
-            break;
-        case "write":
-            mask = SYS_write;
-            break;
-        case "mknod":
-            mask = SYS_mknod;
-            break;
-        case "unlink":
-            mask = SYS_unlink;
-            break;
-        case "link":
-            mask = SYS_link;
-            break;
-        case "mkdir":
-            mask = SYS_mkdir;
-            break;
-        case "close":
-            mask = SYS_close;
-            break;
-        case "ptrace":
-            mask = SYS_ptrace;
-            break;
-        default:
-            // No system call exists
-            printf(2, "Unknown syscall: %s\n", syscall_name);
-            break;
+    if (strcmp(syscall_name, "fork") == 0) {
+        mask = SYS_fork;
+    } else if (strcmp(syscall_name, "exit") == 0) {
+        mask = SYS_exit;
+    } else if (strcmp(syscall_name, "wait") == 0) {
+        mask = SYS_wait;
+    } else if (strcmp(syscall_name, "pipe") == 0) {
+        mask = SYS_pipe;
+    } else if (strcmp(syscall_name, "read") == 0) {
+        mask = SYS_read;
+    }else if (strcmp(syscall_name, "kill") == 0) {
+        mask = SYS_kill;
+    }else if (strcmp(syscall_name, "exec") == 0) {
+        mask = SYS_exec;
+    }else if (strcmp(syscall_name, "fstat") == 0) {
+        mask = SYS_fstat;
+    }else if (strcmp(syscall_name, "chdir") == 0) {
+        mask = SYS_chdir;
+    }else if (strcmp(syscall_name, "dup") == 0) {
+        mask = SYS_dup;
+    }else if (strcmp(syscall_name, "getpid") == 0) {
+        mask = SYS_getpid;
+    }else if (strcmp(syscall_name, "sbrk") == 0) {
+        mask = SYS_sbrk;
+    }else if (strcmp(syscall_name, "sleep") == 0) {
+        mask = SYS_sleep;
+    }else if (strcmp(syscall_name, "uptime") == 0) {
+        mask = SYS_uptime;
+    }else if (strcmp(syscall_name, "open") == 0) {
+        mask = SYS_open;
+    }else if (strcmp(syscall_name, "write") == 0) {
+        mask = SYS_write;
+    }else if (strcmp(syscall_name, "mknod") == 0) {
+        mask = SYS_mknod;
+    }else if (strcmp(syscall_name, "unlink") == 0) {
+        mask = SYS_unlink;
+    }else if (strcmp(syscall_name, "link") == 0) {
+        mask = SYS_link;
+    }else if (strcmp(syscall_name, "mkdir") == 0) {
+        mask = SYS_mkdir;
+    }else if (strcmp(syscall_name, "close") == 0) {
+        mask = SYS_close;
+    }else if (strcmp(syscall_name, "ptrace") == 0) {
+        mask = SYS_ptrace;
+    }else{
+         printf(2, "Sys call does not exist\n");
     }
-
+       
     return mask;
 }
 
@@ -99,8 +74,9 @@ int main(int argc, char *argv[])
     const char *syscall_name = argv[1];
     int mask = call_to_mask(syscall_name);
 
-    int test = ptrace(mask);
+    int test = sys_ptrace(mask);
 
+    printf(2, "Test: %d\n", test);
 
     exit();
 }
